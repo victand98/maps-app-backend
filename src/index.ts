fd578c002641f7abe048241ba0da3df696eedbed
Víctor Andrés Rojas <victandres98@gmail.com>
2021-09-21 16:08:41 -0500

    Update Setup Files

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import log from "./utils/logger";
 const port = config.get("port") as number;
 const host = config.get("host") as string;
 
-app.listen(port, host, () => {
+app.listen(port, () => {
   log.info(`Server started at http://${host}:${port}`);
   connect();
 });
