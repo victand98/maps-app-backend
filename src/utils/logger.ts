87e4b4713d64dfb1071d3338486ee5bb4816a941
Víctor Andrés Rojas <victandres98@gmail.com>
2021-09-21 01:55:52 -0500

    Updating package.json

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,12 @@
-import * as logger from "pino";
-import * as dayjs from "dayjs";
+import logger from "pino";
+import dayjs from "dayjs";
 
-const log = logger.default({
+const log = logger({
   prettyPrint: true,
   base: {
     pid: false,
   },
-  timestamp: () => `,"time":"${dayjs.default().format()}"`,
+  timestamp: () => `,"time":"${dayjs().format()}"`,
 });
 
 export default log;
