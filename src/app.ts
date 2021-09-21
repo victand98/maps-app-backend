462675d0601ed269ab1f683768267499e72a7d4c
Víctor Andrés Rojas <victandres98@gmail.com>
2021-09-20 21:37:32 -0500

    Setting up scripts for production

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import routes from "./routes";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(cors());
 
 routes(app);
 
